77b32824dd4ff5fe9702a1842c735f982d89b810
Khoi Hoang <khoii87@gmail.com>
2021-11-22 02:18:21 +0100

    Add: 📦 Dependency of Formik

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useFormik } from 'formik';
 
 const YouTubeForm = () => {
-    return (
-        <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
 
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" name="email" />
+    const formik = useFormik({});
+    
+	return (
+		<form>
+			<label htmlFor='name'>Name</label>
+			<input type='text' id='name' name='name' />
 
-            <label htmlFor="channel">Channel</label>
-            <input type="text" id="channel" name="channel" />
+			<label htmlFor='email'>E-mail</label>
+			<input type='email' id='email' name='email' />
 
-            <button>Submit</button>
-        </form>
-    )
-}
+			<label htmlFor='channel'>Channel</label>
+			<input type='text' id='channel' name='channel' />
 
-export default YouTubeForm
+			<button>Submit</button>
+		</form>
+	);
+};
+
+export default YouTubeForm;
