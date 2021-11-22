015542ad134d98843e360532a16bdc67eb37f76f
Khoi Hoang <khoii87@gmail.com>
2021-11-22 03:05:07 +0100

    Add: ✨ Validation of input fields has been touched based to give user feedback if it's required or not to write

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -9,26 +9,33 @@ const YouTubeForm = () => {
 		validate, // Validating the form
 	});
 
-	console.log('Form errors', formik.errors);
+	console.log('Visited Fields: ', formik.touched);
 
 	return (
 		<form onSubmit={formik.handleSubmit}>
 			<div className='form-control'>
 				<label htmlFor='name'>Name</label>
-				<input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
-				{formik.errors.name ? <p className='error'>{formik.errors.name}</p> : null}
+				<input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
+				{formik.touched.name && formik.errors.name ? <p className='error'>{formik.errors.name}</p> : null}
 			</div>
 
 			<div className='form-control'>
 				<label htmlFor='email'>E-mail</label>
-				<input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
-				{formik.errors.email ? <p className='error'>{formik.errors.email}</p> : null}
+				<input type='email' id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+				{formik.touched.email && formik.errors.email ? <p className='error'>{formik.errors.email}</p> : null}
 			</div>
 
 			<div className='form-control'>
 				<label htmlFor='channel'>Channel</label>
-				<input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
-				{formik.errors.channel ? <p className='error'>{formik.errors.channel}</p> : null}
+				<input
+					type='text'
+					id='channel'
+					name='channel'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.channel}
+				/>
+				{formik.touched.channel &&  formik.errors.channel ? <p className='error'>{formik.errors.channel}</p> : null}
 			</div>
 
 			<button type='submit'>Submit</button>
