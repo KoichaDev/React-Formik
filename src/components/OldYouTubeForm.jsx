88582ff4e59f60808d6e17ee7028ff869f53f3ab
Khoi Hoang <khoii87@gmail.com>
2021-11-23 02:20:58 +0100

    Add: ✨ Reducing boilder plate of input fields using formik getFieldProps

--- src/components/OldYouTubeForm.jsx
+++ src/components/OldYouTubeForm.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useFormik } from 'formik';
-import { initialValues, onSubmit, validationSchema } from './validate-youtube-form';
+import { initialValues, onSubmit, validate, validationSchema } from './validate-youtube-form';
 
-const YouTubeForm = () => {
+const OldYouTubeForm = () => {
 	const formik = useFormik({
 		initialValues, // initial values and change Handler
 		onSubmit, //Submit the form
+		// validate, // Validating the form
 		validationSchema, // Validating by using Yup packages
 	});
 
@@ -15,19 +16,26 @@ const YouTubeForm = () => {
 		<form onSubmit={formik.handleSubmit}>
 			<div className='form-control'>
 				<label htmlFor='name'>Name</label>
-				<input type='text' id='name' name='name' {...formik.getFieldProps('name')} />
+				<input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
 				{formik.touched.name && formik.errors.name ? <p className='error'>{formik.errors.name}</p> : null}
 			</div>
 
 			<div className='form-control'>
 				<label htmlFor='email'>E-mail</label>
-				<input type='email' id='email' name='email' {...formik.getFieldProps('email')} />
+				<input type='email' id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
 				{formik.touched.email && formik.errors.email ? <p className='error'>{formik.errors.email}</p> : null}
 			</div>
 
 			<div className='form-control'>
 				<label htmlFor='channel'>Channel</label>
-				<input type='text' id='channel' name='channel' {...formik.getFieldProps('channel')} />
+				<input
+					type='text'
+					id='channel'
+					name='channel'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.channel}
+				/>
 				{formik.touched.channel && formik.errors.channel ? <p className='error'>{formik.errors.channel}</p> : null}
 			</div>
 
@@ -36,4 +44,4 @@ const YouTubeForm = () => {
 	);
 };
 
-export default YouTubeForm;
+export default OldYouTubeForm;
