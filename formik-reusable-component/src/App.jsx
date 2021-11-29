7afe897fce93ebf528f7e265f4488e7a310eddf1
Khoi Hoang <khoii87@gmail.com>
2021-11-29 21:05:54 +0100

    Add: ✨ EnrollmentForm

--- formik-reusable-component/src/App.jsx
+++ formik-reusable-component/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import FormikContainer from './components/FormikControl/FormikContainer';
 import RegistrationForm from './components/Form/RegistrationForm';
 import LoginForm from './components/Form/LoginForm';
+import EnrollmentForm from './components/Form/EnrollmentForm'
 import './App.css';
 
 const App = () => {
 	return (
 		<div className='App'>
+			<EnrollmentForm />
 			{/* <FormikContainer /> */}
-			<RegistrationForm />
+			{/* <RegistrationForm /> */}
 			{/* <LoginForm /> */}
 		</div>
 	);
